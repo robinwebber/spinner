afdbfaa36f057da933df3c7a97b8c16d616d4fe5
robinwebber <robinewebber@gmail.com>
2019-07-02 17:56:15 +0000

    added spinner2.js

---
+++ spinner2.js
@@ -0,0 +1,15 @@
+// Pair coded with Hans Xu
+
+
+const spinnerArray = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|'];
+const spinner = function (time, arr) {
+
+  for (let i = 0; i < arr.length; i++) {
+    setTimeout(() => {
+      process.stdout.write('\r' + arr[i] + ' ');
+    }, 100 + (i * time));
+  }
+
+};
+
+spinner(100, spinnerArray);
